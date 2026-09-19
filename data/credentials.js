322d9b83aa1e6a440693a01eb816ed8dbad48862
in00000 <129388071+in00000@users.noreply.github.com>
2026-09-19 13:11:12 +0530

    Update credentials.js

--- data/credentials.js
+++ data/credentials.js
@@ -1,25 +1,23 @@
 /* ============================================================
    15 LOGIN ACCOUNTS
    Username: PD45981 ... PD45995
-   Passwords: unique, non-sequential, India-themed
-
-   To change a password, just edit the "p" field below.
-   To add/remove users, follow the same {u, p} pattern.
+   Password format: pcmcdiary + unique 4-digit code
    ============================================================ */
+
 const USERS = [
-  { u: "PD45981", p: "ShreeGanesh@25A" },
-  { u: "PD45982", p: "JaiBhavani@48K" },
-  { u: "PD45983", p: "OmSaiRam@73Q" },
-  { u: "PD45984", p: "GanpatiDeva@12M" },
-  { u: "PD45985", p: "JaiMaharashtra@89P" },
-  { u: "PD45986", p: "ShivbaRaje@36D" },
-  { u: "PD45987", p: "KirtiBhushan@57H" },
-  { u: "PD45988", p: "SatyaShodhak@91R" },
-  { u: "PD45989", p: "NamoBuddha@64T" },
-  { u: "PD45990", p: "JaiHindBharat@18W" },
-  { u: "PD45991", p: "VishwaShanti@42B" },
-  { u: "PD45992", p: "RajmudraSwarajya@75E" },
-  { u: "PD45993", p: "SwarajyaSthapak@29F" },
-  { u: "PD45994", p: "AnandSagar@83J" },
-  { u: "PD45995", p: "PunyaNagar@56N" }
+  { u: "PD45981", p: "pcmcdiary4827" },
+  { u: "PD45982", p: "pcmcdiary7314" },
+  { u: "PD45983", p: "pcmcdiary9062" },
+  { u: "PD45984", p: "pcmcdiary1589" },
+  { u: "PD45985", p: "pcmcdiary6240" },
+  { u: "PD45986", p: "pcmcdiary3975" },
+  { u: "PD45987", p: "pcmcdiary8406" },
+  { u: "PD45988", p: "pcmcdiary2751" },
+  { u: "PD45989", p: "pcmcdiary5683" },
+  { u: "PD45990", p: "pcmcdiary9148" },
+  { u: "PD45991", p: "pcmcdiary3469" },
+  { u: "PD45992", p: "pcmcdiary8073" },
+  { u: "PD45993", p: "pcmcdiary2516" },
+  { u: "PD45994", p: "pcmcdiary6792" },
+  { u: "PD45995", p: "pcmcdiary4385" }
 ];
